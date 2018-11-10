e4aae7420bbf2c74ef845160e402029141c3e364
Andrew Longhorn <>
2018-11-10 16:14:46 +1000

    initial version

---
+++ Allow2/Allow2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Allow2
+{
+    public class Connection
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public Connection()
+        {
+        }
+
+        public async Task<string> test()
+        {
+
+            //var values = new Dictionary<string, string>
+            //    {
+            //       { "thing1", "hello" },
+            //       { "thing2", "world" }
+            //    };
+
+            //var content = new FormUrlEncodedContent(values);
+
+            //var response = await client.PostAsync("http://api.allow2.com/", content);
+
+            //var responseString = await response.Content.ReadAsStringAsync();
+
+            var responseString = await client.GetStringAsync("http://api.allow2.com/");
+
+            return responseString;
+        }
+    }
+}
